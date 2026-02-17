08b3ae2d24f59d12938162bb01ebafee7cb5315e
Aman-11008599 <aman@uppcl.org>
2026-02-17 23:17:31 +0530

    moved the slideshow to page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,12 @@
+import Slideshow from "@/components/slideshow/Slideshow";
+
 export default function HomePage() {
   return (
-    <section className="container" style={{ padding: "20px 0" }}>
-      <h2>Welcome to UPPCL</h2>
-      <p>
-        Uttar Pradesh Power Corporation Limited is responsible for power
-        transmission and distribution within the state.
-      </p>
-    </section>
+    <main>
+
+      {/* HOME PAGE ONLY */}
+      <Slideshow />
+
+    </main>
   );
 }
